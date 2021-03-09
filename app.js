0b12ef3fc811564313c41a4b5d91e164794cd41d
DimaVnuk <vnuchok311994@hotmail.com>
2021-03-09 16:18:16 +0300

    add homework

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict'
+
+let object = {
+    name: 'Dima',
+    surname: 'Vnuk',
+    age: 18,
+}
+
+let name = 'Constantine';
+
+object.sex = 'male';
+object.nationality = 'Belarusian';
+
+object.sayHello = function () {
+    return this.name + ' hello'
+};
+
+object.sayGoodbye = function () {
+    return 'Goodbye ' + name
+};
+
+object.stringKeyValue = function () {
+    for (let key in object) {
+        console.log(key + ': ' + object[key]);
+    }
+}
+
+let checkObject = function (newObject) {
+    for (let key in newObject) {
+        if (typeof newObject[key] === 'object' && newObject[key] !== null) {
+            return true
+        }
+    }
+    return false
+}
+
